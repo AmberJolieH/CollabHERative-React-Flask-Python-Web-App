6224de32f685c22e4e9ffa68a40f1e1ae16c247f
amberjolie <amberjolieh@gmail.com>
2021-03-04 18:56:54 -0600

    skillshowcase

---
+++ app/models/skillshowcase.py
@@ -0,0 +1,21 @@
+from .db import db
+
+
+class Skillshowcase(db.Model):
+    __tablename__ = "skillshowcases"
+
+    id = db.Column(db.Integer, primary_key=True)
+    showcaseid = db.Column(db.Integer, db.ForeignKey("showcases.id"), nullable=False)
+    skillsid = db.Column(db.Integer, db.ForeignKey("skills.id"), nullable=False)
+    
+
+
+
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "showcaseid": showcaseid,
+            "skillsid": self.skillsid
+            
+        }
