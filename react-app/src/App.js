39bedebe727e297cd660bf0b82c07a0001a21820
amberjolie <amberjolieh@gmail.com>
2021-03-05 18:22:31 -0600

    redux store and service

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import LoginForm from "./components/auth/LoginForm";
 import SignUpForm from "./components/auth/SignUpForm";
 import NavBar from "./components/NavBar";
+import CreateShowcase from "./components/showcase/createShowcase"
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import UsersList from "./components/UsersList";
 import User from "./components/User";
@@ -42,6 +43,13 @@ function App() {
         <ProtectedRoute path="/users" exact={true} authenticated={authenticated}>
           <UsersList/>
         </ProtectedRoute>
+        <ProtectedRoute
+              path="/showcase/create_showcase"
+              exact={true}
+              authenticated={authenticated}
+            >
+              <CreateShowcase/>
+            </ProtectedRoute>
         <ProtectedRoute path="/users/:userId" exact={true} authenticated={authenticated}>
           <User />
         </ProtectedRoute>
--- react-app/src/components/showcase/createShowcase.js
+++ react-app/src/components/showcase/createShowcase.js
@@ -3,14 +3,75 @@ import React, {useState} from "react";
 import { jsx } from "@emotion/react";
 import { createshowcase } from "../../services/showcase";
 
-const createShowcase = () =>{
+const CreateShowcase = () => {
     const [techCategoryId, setTechCategoryId] = useState('')
     const [description, setDescription] = useState('')
     const [skill,setSkills] = useState('')
+    const [showcaseImgUrl, setshowcaseImgUrl] = useState('')
+
+
+const onSubmit = async (e)=> {
+    e.preventDefault()
+    const showcase = await createshowcase({
+        techCategoryId,
+        description,
+        skill,
+        showcaseImgUrl
+    })
+}
+    console.log('test showcase create working')
+
+    
+
+    const cat = [
+        'UX/UI Design',
+        'Product Marketing/Product Management',
+        'Software (Full Stack/Front End/Back End)',
+        'Cloud Computing',
+        'Cybersecurity',
+        'Data Analytics/Data Science',
+        'Tech Sales/Tech Procurement',
+        'AI/Machine Learning/Automation',
+    ]
+ const [CatName, setCatName] = useState(cat[0])
+  
+ return (
+        <div>
+            <h2>Create a showcase</h2>
+            <form
+            onSubmit={onSubmit}
+                
+                >
+                    <label>Description: </label>
+                <input
+                    name="description"
+                    type="text"
+                    placeholder="description of showcase"
+                    value={description}
+                    onChange={e => setDescription(e.target.value)}
+                />
+                <label>Select a relative tech category: </label>
+                <select
+                    name="techCatName"
+                    type="text"
+                    value={cat}
+                    onChange={e => setCatName(e.target.value)}
+                >
+                {techCategoryId.map((cat)=> {
+                    return (
+                        <option
+                        key={cat}
+                        value={cat}
+                        >
+                            {cat}
+                        </option>
+                    )
+                })}
+                </select>
+                <button>post showcase</button>
+            </form>
+        </div>
+    )
 }
 
-const onSubmit = async (e)=> 
-    e.preventDefault()
-    const resource = await createshowcase({
-        techCategoryId
-    })
+export default createshowcase;
