c6e9ffb4b3cb780d96235b9b72c74a7954a7a18e
amberjolie <amberjolieh@gmail.com>
2021-03-09 13:01:38 -0600

    nav update

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -6,42 +6,42 @@ import logo from "../images/CollabHERativelogo2.svg"
 const NavBar =({ setAuthenticated, authenticated }) => {
   return (
     <nav>
-      <ul>
-        <li>
+      <div>
+        <div>
           <NavLink to="/" exact={true} activeClassName="active">
             <img src={logo} alt="React Logo" style={{ display:"flex", }} />
           </NavLink>
 
-        </li>
-        <li>
+        </div>
+        <div>
           <NavLink to="/login" exact={true} activeClassName="active">
             Login
           </NavLink>
-        </li>
-        <li>
+        </div>
+        <div>
           <NavLink to="/sign-up" exact={true} activeClassName="active">
             Sign Up
           </NavLink>
-        </li>
-        <li>
+        </div>
+        <div>
           <NavLink to="/users" exact={true} activeClassName="active">
             Users
           </NavLink>
-        </li>
-          <li>
+        </div>
+          <div>
           <NavLink to="/create_showcase" exact={true} activeClassName="active">
             Create a Showcase
           </NavLink>
-        </li>
-        <li>
+        </div>
+        <div>
           <NavLink to="/all_showcases" exact={true} activeClassName="active">
             view all Showcases
           </NavLink>
-        </li>
-        <li>
+        </div>
+        <div>
           <LogoutButton setAuthenticated={setAuthenticated} />
-        </li>
-      </ul>
+        </div>
+      </div>
     </nav>
   );
 }
