df595d61f7aa707ea0218313e73b06fc823d6b70
amberjolie <amberjolieh@gmail.com>
2021-03-22 17:27:56 -0500

    py-bk-setup

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import {useDispatch} from "react-redux"
 
+
+
 //* COMPONENTS 
 import LoginForm from "./components/auth/LoginForm";
 import SignUpForm from "./components/auth/SignUpForm";
