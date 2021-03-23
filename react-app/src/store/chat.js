7cf47edaf3574f4231d5541d3f45047544f38b1f
amberjolie <amberjolieh@gmail.com>
2021-03-23 17:18:40 -0500

    store

--- react-app/src/store/chat.js
+++ react-app/src/store/chat.js
@@ -19,3 +19,89 @@ const remove = (messageId) => ({
   messageId,
 });
 
+//* REDUX -> THUNK -> JSON FETCH
+
+// export const getMessages = () => async (dispatch) => {
+//   const res = await fetch('/api/chatData');
+//   const json = await res.json();
+//   if (res.ok) {
+//     dispatch(load(json.messages));
+//   }
+// };
+
+//* CREATE A NEW MESSAGE 
+
+export const createMessage = (newMessage) => async (dispatch) => {
+  const { senderId, receiverId, message } = newMessage;
+
+  const res = await fetch(`/api/chatData`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      senderId,
+      receiverId,
+      message,
+    }),
+  });
+  const returnedMessage = await res.json();
+
+  if (!returnedMessage.errors) {
+    dispatch(create(returnedMessage));
+    return returnedMessage;
+  } else {
+    const errors = returnedMessage;
+    return errors;
+  }
+};
+
+//* DELETE MESSAGE
+
+export const deleteMessage = (messageId) => async (dispatch) => {
+  const res = await fetch(`/api/messages/${messageId}`, {
+    method: 'DELETE',
+  });
+  if (res.ok) {
+    dispatch(remove(messageId));
+  }
+};
+
+//* REDUCER
+
+const initState = {
+  1: {
+    senderId: 1,
+    receiverId: 2,
+    message: '',
+    timestamp: '',
+    sender: {
+      username: '',
+    },
+    receiver: {
+      username: '',
+    },
+  },
+};
+
+const chatReducer = (state = initState, action) => {
+  const newState = { ...state };
+
+  switch (action.type) {
+    case LOAD_MESSAGES:
+      for (let message of action.messages) {
+        newState[message.id] = message;
+      }
+      return newState;
+    case CREATE_MESSAGE:
+      newState[action.message.id] = action.message;
+      return newState;
+    case REMOVE_MESSAGE:
+      delete newState[Number(action.messageId)];
+      return newState;
+    default:
+      return newState;
+  }
+};
+
+export default chatReducer;
