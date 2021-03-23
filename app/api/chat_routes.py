7c13040de77a44c65bc58008f6303506a0aa8ea8
amberjolie <amberjolieh@gmail.com>
2021-03-23 15:55:08 -0500

    routes

--- app/api/chat_routes.py
+++ app/api/chat_routes.py
@@ -1,14 +1,36 @@
+# IMPORTS
 from app.models import DirectMessage
-from flask import Blueprint
-chat_routes = Blueprint('chat',__name__)
+from flask import Blueprint, jsonify, request
 from flask_login import login_required
+# BLUEPRINT
+chat_routes = Blueprint('chat', __name__)
+
+# GET ALL MESSAGES
 
 
-@chat_routes.route('/users/<int:id>')
+# @chat_routes.route('/users/<int:id>')
+# @login_required
+# def getChat(id):
+#     messages = DirectMessage.query.filter_by(chat_id=id).all()
+#     return {id: [message.to_dict() for message in messages]}
+
+@chat_routes.route("")
 @login_required
-def getChat(id):
-    messages = DirectMessage.query.filter_by(chat_id=id).all()
-    return {id: [message.to_dict() for message in messages]}
+def get_chats():
+    """
+    Get all messages
+    """
+    messages = Message.query.all()
+    return {"messages": [message.to_dict() for message in messages]}
+
+# CREATE A NEW MESSAGE
 
 
-    
+@chat_routes.route("", methods=["POST"])
+@login_required
+def create_chat():
+    """
+    Create new message
+    """
+    form = CreateMessageForm()
+    form["csrf_token"].data = request.cookies["csrf_token"]
