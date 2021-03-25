dd377975c1f49a7d307762d20c9947720e35600c
amberjolie <amberjolieh@gmail.com>
2021-03-24 23:03:14 -0500

    herokupush

--- react-app/src/components/chat/chatWindow.js
+++ react-app/src/components/chat/chatWindow.js
@@ -1,6 +1,6 @@
 //* IMPORTS 
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import CenterCard from '../centerCard/centerCard';
 
 //* CONTEXT
@@ -15,9 +15,24 @@ import { deleteMessage } from '../../store/chat';
 //*CSS
 import './chat.css'
 
-export default function ChatWindow({ lgdInUser, allChatsWOtherUser }) {
+export default function ChatWindow({ lgdInUser }) {
   const { otherUser } = useOtherUserContext();
   const dispatch = useDispatch();
+  const allChats = useSelector((state) => state.chat);
+
+
+  const chatsArray = Object.values(allChats);
+  console.log(chatsArray)
+  const allChatsForUser = chatsArray.filter(
+    (message) =>
+      message.senderid === lgdInUser.id || message.receiverid === lgdInUser.id
+  );
+console.log(allChatsForUser)
+
+  const allChatsWOtherUser = allChatsForUser.filter((message) => {
+    const idToCheck = otherUser.id;
+    return message.senderid === idToCheck || message.receiverid === idToCheck;
+  });
 
   const handleDelete = function (msg) {
     const res = window.confirm(`Delete this message? "${msg.message}"`);
@@ -65,7 +80,7 @@ export default function ChatWindow({ lgdInUser, allChatsWOtherUser }) {
             allChatsWOtherUser.map((msg) => (
               <div
                 className={
-                  lgdInUser.id === msg.sender.id
+                  lgdInUser.id === msg.senderid
                     ? 'chat__texts__right'
                     : 'chat__texts__left'
                 }
@@ -73,7 +88,7 @@ export default function ChatWindow({ lgdInUser, allChatsWOtherUser }) {
               >
                 <p
                   style={
-                    lgdInUser.id === msg.sender.id
+                    lgdInUser.id === msg.senderid
                       ? {
                           background: 'rgba(13, 51, 223, 0.65)',
                         }
@@ -81,7 +96,7 @@ export default function ChatWindow({ lgdInUser, allChatsWOtherUser }) {
                   }
                   className='single-chat-text'
                   title={msg.sender.firstname}
-                  onClick={lgdInUser.id === msg.sender.id ? () => handleDelete(msg) : undefined}
+                  onClick={lgdInUser.id === msg.senderid ? () => handleDelete(msg) : undefined}
                 >
                   {msg.message}
                 </p>
